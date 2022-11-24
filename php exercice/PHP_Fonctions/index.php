25a27a383185addd6d995f7fb57a072c92055367
Collin Guillaume <220053@CCM-Asty-Moulin.local>
2022-11-24 14:31:32 +0100

    edenszero

---
+++ php exercice/PHP_Fonctions/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP_Fonctions</title>
+</head>
+<body>
+    <?php 
+    $nb1=7;
+    function factorielle ($nb1)
+    {
+        $n1=1;
+        $n2=1;
+            while( $n2  $nb1)
+
+    }
+    ?>
+</body>
+</html>
